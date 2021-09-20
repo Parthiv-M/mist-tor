f996f5053861a25f5d4ef453a3efcf95abb87d15
Parthiv-M <parthiv.menon@learner.manipal.edu>
2021-09-20 20:28:57 +0530

    included tailwind and updated directory structure

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App () {
+  return (
+    <div className="App">
+      <p className="text-gray-500 hover:text-gray-900">Hello Tailwind</p>
+    </div>
+  );
+}
+
+export default App;
