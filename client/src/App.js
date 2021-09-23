493f7234452177b8a12584adcd6f5f56923899f7
Parthiv-M <parthiv.menon@learner.manipal.edu>
2021-09-23 19:38:28 +0530

    added context and routes config

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import routes from './config/routes';
+import { TorProvider } from './context';
+import AppRoute from "./AppRoutes";
 
 function App () {
   return (
     <div className="App">
-      <p className="text-gray-500 hover:text-gray-900">Hello Tailwind</p>
+      <TorProvider>
+        <Router>
+          <Switch>
+            {
+              routes.map((route) => (
+                <AppRoute
+                  key={route.path}
+                  path={route.path}
+                  component={route.component}
+                  isPrivate={route.isPrivate}
+                />
+              ))
+            }
+          </Switch>
+        </Router>
+      </TorProvider>
     </div>
   );
 }
---
+++ client/src/context/index.js
@@ -0,0 +1,9 @@
+import { signUpUser } from './actions';
+import { TorProvider, useTorDispatch, useTorState } from './context';
+ 
+export { 
+    TorProvider, 
+    useTorState, 
+    useTorDispatch,  
+    signUpUser
+};
