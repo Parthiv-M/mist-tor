fe111a9a11588484c66ae03bc068e2d6e39706e6
harshikasofat14 <harshikasofat14@gmail.com>
2021-10-02 12:43:07 +0530

    Create LoginPage.js

---
+++ client/src/pages/LoginPage.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import './LoginPage.css';
+import align0 from "./images/Frame copy.png";
+import align01 from "./images/Frame.png";
+import align02 from "./images/Frame-3.png";
+import align03 from "./images/Group.png";
+import align1 from "./images/Vector copy.png";
+import align2 from "./images/Vector copy 2.png";
+import align3 from "./images/Vector copy 3.png";
+import align4 from "./images/Vector.png";
+import align5 from "./images/Vector-2.png";
+import align06 from "./images/Group-2 copy.png";
+import align6 from "./images/Group-2 copy 2.png";
+import align7 from "./images/Vector-3.png";
+import align8 from "./images/Vector-4.png";
+import align9 from "./images/Frame-2.png";
+
+const LoginPage = () => {
+    return (
+        <div className ="wrapper">
+          <div className = "align0">
+<img src={align0}  align="left" />
+</div>
+<div className = "align01">
+<img src={align01}  align="right" />
+</div>
+<div className="topnav" id="myTopnav">
+  <a href="#news">BLOG</a>
+  <a href="#contact">NEWS</a>
+  <a href="#about">TEAM</a>
+</div>
+
+<div className = "align02">
+<img src={align02}  align="left" />
+</div>
+<div className = "align03">
+<img src={align03}  align="right" />
+</div>
+<div className = "align1 ">
+<img src={align1} align="left" />
+</div>
+<div className = "align2 ">
+<img src={align2} align="left" />
+</div>
+<div className = "align3 ">
+<img src={align3} align="left" />
+</div>
+<div className = "align4 ">
+<img src={align4} align="left" />
+</div>
+<div className = "align5 ">
+<img src={align5} align="left" />
+</div>
+<div className = "align06 ">
+<img src={align06} />
+</div>
+<div className="align6">
+<img src={align6} />
+</div>
+<div className ="align7">
+<img src={align7} align="right" />
+</div>
+<div className="align8">
+<img src={align8} align="right" />
+</div>
+<div className = "align9 ">
+<img src={align9}  align="center" />
+</div>
+</div>
+    )
+}
+
+export default LoginPage;
