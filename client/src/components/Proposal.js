ba6fd086596e79754ce16bcc63f1d0c036b75930
Ayush Vatsa <64834401+mrvatsa@users.noreply.github.com>
2021-10-11 00:52:39 +0530

    Create Proposal.js

---
+++ client/src/components/Proposal.js
@@ -0,0 +1,42 @@
+import React from "react"
+const Proposal = () => {
+   return (
+       <div><div class="mainCard  rounded-none sm:rounded-lg max-w-3xl">
+        <div class="closeBtn px-2 py-2 sm:px-3 sm:py-4">
+            <button type="button" class="close bg-#fffff px-2">
+             <span class="sr-only">Close menu</span>
+             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-circle">
+                <circle cx="12" cy="12" r="10"></circle>
+                <line x1="15" y1="9" x2="9" y2="15"></line>
+                <line x1="9" y1="9" x2="15" y2="15"></line></svg>
+           </button>
+        </div>
+            <div class="px-10 py-9 sm:px-15 sm:py-0">
+                <div class="grid-rows-1">
+                    <input class="Box px-3" type="text" size="30" placeholder="Subject">
+                </div>
+                <div class="grid-rows-1">
+                 <textarea class="Box px-3" type="text" rows="5" cols="50" placeholder="Write your Proposal here..."></textarea>
+                </div>
+               
+                <div class="px-5 py-3 sm:px-0 sm:py-0">
+                    <button class="btn-upload text-white py-1 px-1 text-l mr-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-upload">
+                        <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                        <polyline points="17 8 12 3 7 8"></polyline>
+                        <line x1="12" y1="3" x2="12" y2="15"></line></svg>
+                    </button>
+                </div>
+                <div class="px-10 py-6 sm:px-8 sm:py-0">
+                    <span class="upload">Related files (if any)</span>
+                </div> 
+                <div  class="grid-rows-1">
+                 <button class="btn-propose text-white font-weight:900 py-2 px-6 text-l mr-4">Post</button>
+            </div>
+
+        </div>
+    </div>
+  </div>
+    )
+}
+export default Proposal
