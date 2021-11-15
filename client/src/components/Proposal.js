c202112c595230e18a8cd7f7a6127e7a968d021a
Ayush Vatsa <64834401+mrvatsa@users.noreply.github.com>
2021-11-16 03:05:45 +0530

    Update Proposal.js

--- client/src/components/Proposal.js
+++ client/src/components/Proposal.js
@@ -1,7 +1,7 @@
 import React from "react"
 const Proposal = () => {
    return (
-       <div><div class="mainCard  rounded-none sm:rounded-lg max-w-3xl">
+       <div><div class="mainCard  rounded-none sm:rounded-lg max-w-3xl m-auto h-auto bg-gray-800 text-white mt-10">
         <div class="closeBtn px-2 py-2 sm:px-3 sm:py-4">
             <button type="button" class="close bg-#fffff px-2">
              <span class="sr-only">Close menu</span>
@@ -13,14 +13,14 @@ const Proposal = () => {
         </div>
             <div class="px-10 py-9 sm:px-15 sm:py-0">
                 <div class="grid-rows-1">
-                    <input class="Box px-3" type="text" size="30" placeholder="Subject">
+                    <input class="Box rounded-lg px-3 mt-5 mb-5 ml-auto mr-auto border-2 w-full p-2" type="text" size="30" placeholder="Subject">
                 </div>
                 <div class="grid-rows-1">
-                 <textarea class="Box px-3" type="text" rows="5" cols="50" placeholder="Write your Proposal here..."></textarea>
+                 <textarea class="Box px-3 rounded-lg mt-5 mb-5 ml-auto mr-auto border-2 w-full p-2" type="text" rows="5" cols="50" placeholder="Write your Proposal here..."></textarea>
                 </div>
                
                 <div class="px-5 py-3 sm:px-0 sm:py-0">
-                    <button class="btn-upload text-white py-1 px-1 text-l mr-4">
+                    <button class="btn-upload text-white py-1  px-1 rounded-xl text-l mr-4 mt-auto ml-0 fixed">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-upload">
                         <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
                         <polyline points="17 8 12 3 7 8"></polyline>
@@ -28,10 +28,10 @@ const Proposal = () => {
                     </button>
                 </div>
                 <div class="px-10 py-6 sm:px-8 sm:py-0">
-                    <span class="upload">Related files (if any)</span>
+                    <span class="upload inline mt-auto p-1 text-base text-gray-400">Related files (if any)</span>
                 </div> 
                 <div  class="grid-rows-1">
-                 <button class="btn-propose text-white font-weight:900 py-2 px-6 text-l mr-4">Post</button>
+                 <button class="btn-propose text-white rounded-lg shadow-2xl mb-10 font-extrabold mr text-white font-weight:900 py-2 px-6 text-l mr-4">Post</button>
             </div>
 
         </div>
