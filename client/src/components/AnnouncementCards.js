1665f98c1d33ceedb162e65d3718c684c60d2ef4
handaakshit05 <77069564+handaakshit05@users.noreply.github.com>
2021-11-25 08:19:30 +0530

    Update AnnouncementCards.js

--- client/src/components/AnnouncementCards.js
+++ client/src/components/AnnouncementCards.js
@@ -1,88 +1,51 @@
+import Button from '@restart/ui/esm/Button';
 import React from 'react'
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { Button, Breadcrumb, BreadcrumbItem, Container, Row, Col, ProgressBar } from "react-bootstrap"
-import { Card, CardImg, } from "react-bootstrap"
-
-const cardbody = {
-    background: "#3C3644",
-    boxShadow: "0px 0px 15px 4px rgba(0, 0, 0, 0.25)",
-    borderRadius: "9px", 
-    margin: "0 px",
-    padding: "0 px",
-    display: 'flex',
-    flex: "1",
-    flexDirection: "Column",
-    justifyContent: "center",
-    alignItems: "center",
-};
-
-const button = {
-    background: "#221F26",
-    boxshadow: "0px 0px 15px 4px rgba(0, 0, 0, 0.25)",
-    borderradius: "9px",
-    border: "none",
-};
-
-const horizontalline = {
-    width: "213.02px",
-    height: "0 px",
-    border: "1px solid #7D4696",
-    padding: "none",
-    margin: "none",
-};
-const date = {
-
-    width: "163px",
-    height: "20px",
+const Heading = {
     fontFamily: "Ubuntu",
-    fontStyle: "normal",
-    fontWeight: "normal",
-    fontSize: "15px",
-    lineHeight: "17px",
-    color: "#F5F5F5",
-
+    color: "#FFFFFF",
 };
-const Title = {
-    width: "163px",
-    height: "20px",
+
+const Date = {
     fontFamily: "Ubuntu",
-    fontStyle: "normal",
-    fontWeight: "normal",
-    fontSize: "20px",
-    lineHeight: "23px",
-    color: "#F5F5F5",
+    color: "#FFFFFF",
 
 };
-
 const Text = {
-    width: "411px",
-    height: "71px",
-
     fontFamily: "Ubuntu",
-    fontStyle: "normal",
-    fontWeight: "normal",
-    fontSize: "18px",
-    lineHeight: "21px",
-    textAlign: "center",
-
-    color: "#F5F5F5",
+    color: "#FFFFFF",
 
 };
+const Btn = {
 
+
+};
 
 const AnnnouncementCards = () => {
     return (
-        <Card className="text-center">
-            <Card.Body style={cardbody}>
-                <Card.Title style={Title}>Announcement</Card.Title>
-                <div style={horizontalline}></div>
-                <div style={date}>Date</div>
-                <Card.Text style={Text}>
-                    This is announcement. This is announcement. This is very important.
-                </Card.Text>
-                <Button style={button}>Full Announcement</Button>
-            </Card.Body>
-        </Card>);
+        <div class="w-screen h-full rounded-md shadow-xl flex flex-col justify-center items-center" style={{ backgroundColor: "#3C3644" }} >
+            <div>
+                <p class="text-xl" style={Heading}>
+                    Annnouncement 5
+                </p>
+            </div>
+            <div class="h-px w-52" style={{ backgroundColor: "#7D4696" }}>
+            </div>
+            <div class="text-base" style={Date}>
+                20th Feb, 2020
+            </div>
+            <div>
+                <p class="text-lg" style={Text}>
+                    This is announcement number 5. The number of this announcement is 5.
+                </p>
+            </div>
+            <button class="border-2 rounded-3xl" style={Btn}>
+                <p class="text-xl" style={Heading}>
+                    Full Annnouncement
+                </p>
+            </button>
+        </div>
+
+    );
 }
 export default AnnnouncementCards;
