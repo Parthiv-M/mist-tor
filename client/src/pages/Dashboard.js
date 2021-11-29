0c906b1009e3d635de9b750eb11682a2006141a7
maximus <nittala.manish@learner.manipal.edu>
2021-11-29 14:06:29 +0530

    first commit

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,64 +1,60 @@
 import React from "react"
 const Dashboard = () => {
-   return (
-       <div><button class="btn1"> DASHBOARD</button>
-    <div class="box">
-       <div class="the">the</div>
-        <div class="heading"> M I S T</div>
-        <div class="net"> tor network</div>
-        <div class="line2"></div>
-        <div class="line3"></div>
-        <div class="ip">
-            <div> IPv4 127.0.0.1 </div>
-            <div>IPv6 ::1</div>
-        </div>
+    return (
+       <div>
+            <button style={{ backgroundColor: "#3C3644" }} classname="p-2 mt-4 ml-6 rounded-lg text-white font-bold "> DASHBOARD</button>
+    <div style={{backgroundColor: "#59316B",height: "500px"}} classname="mt-4 p-5">
+        <div classname="text-white text-4xl absolute  ">the</div>
+                <div style={{ marginTop: "30px"}} classname="text-white text-7xl absolute "> M I S T</div>
+                <div style={{ marginTop: "95px"}} classname="text-white text-4xl absolute "> tor network</div>
+                <div style={{ marginTop: "135px" }} classname="text-white text-lg absolute tracking-widest"></div>
+                    <div classname="w-48 h-0.5" style={{ backgroundColor: "white" }}></div>
+                    <div classname="w-60 h-0.5 mt-1" style={{ backgroundColor: "#7D4696" }}></div>
+             <div> IPv4 127.0.0.1 </div>
+             <div>IPv6 ::1</div>
+         </div>
+                <img src="logoLight 2.png" style={{ position: "absolute", right: "0px", height: "400px", marginTop: "10px" }}/>
+                <img src="wave2.png" style={{ position: "absolute", right: "0px", marginTop: "200px", width: "1300px" }}/>
+                <img src="wave 1.png" style={{ position: "absolute", right: "0px", marginTop: "190px" }}/>
+                <img src="wave 1.png" style={{ position: "absolute", right: "0px", marginTop: "210px" }}/>
+                <img src="EditProfile.png" style={{ position: "absolute", right: "0px", height: "260px", marginTop: "250px", marginRight: "20px" }}/>
+                <img src="circle 1.png" style={{ position: "absolute", right: "0px", height: "240px", marginTop: "260px", marginRight: "30px" }}/>
+                <img src="Vector.png" style={{ position: "absolute", right: "0px", marginTop: "385px", marginRight: "80px" }}/>
+                <img src="Vector (1).png" style={{ position: "absolute", right: "0px", marginTop: "305px", marginRight: "115px" }}/>
 
-        <img src="logoLight 2.png" style="position: absolute; right: 0px;height: 500px;margin-top: 10px;">
-        <img src="wave2.png" style="position: absolute; right: 0px; margin-top: 190px;width: 1300px;">
-        <img src="wave 1.png" style="position: absolute; right: 0px;margin-top: 190px;">
-        <img src="wave 1.png" style="position: absolute; right: 0px;margin-top: 210px;">
-        <img src="EditProfile.png" style="position: absolute; right: 0px;height: 260px;margin-top:250px;margin-right: 20px;">
-        <img src="circle 1.png" style="position: absolute; right: 0px;height: 240px;margin-top:260px;margin-right: 30px;">
-        <img src="Vector.png" style="position: absolute; right: 0px;margin-top:385px;margin-right: 80px;">
-        <img src="Vector (1).png" style="position: absolute; right: 0px;margin-top:305px;margin-right: 115px;">
-        
-    </div>
-  
-    
-    <h3 class="para">WORKING COMMITTEE MEMBER</h3>
-    <div class="line"></div>
- 
-
-    <div class="middle">
-    <div class="para2 ">I have done my schooling from dolukpur. For my graduation, I chose Xyz program at Abc University. It was a very enriching experience at the University as not only we were actively involved in practical projects, but also got opportunities to participate in a number of sports and other extra-curricular activities such as plays and skits.</div>
-    <div>
-        <button class="btn2"> <i class="bi bi-github"></i>  GitHub</button>
-        <button class="btn2"><i class="bi bi-envelope"></i>   Email</button>
-        <button class="btn2"> <i class="bi bi-instagram"></i> Instagram</button>
-       </div>
-    </div>
-      <div class="middle2">
-          <button class="btn3"> proposal</button>
-          <button class="btn3"> news</button>
-          <button class="btn3"> complain</button>
-      </div>
-      <button class="btn4"> CREATE ANNOUNCEMENTS</button>
-      <div class="footer">
-        <img src="mist.png" style="height: 45%;margin-top: 40px;margin-left: 60px; ">
-        <div> 
-            <div class="para3">Manipal Information Security Team</div>
-            <div class="para4">Manipal Institute Of Technology Udupi, Karnataka 576104</div>
+                <div classname="text-white text-2xl relative" style={{ marginTop: "500px" }}>MANCOMM MEMBER</div>
+                <div classname="w-72 h-0.5 " style={{ backgroundColor: "#7D4696 " }}></div>
+         <div classname="grid grid-cols-3">
+         <div classname="text-white text-lg col-span-2 ">I have done my schooling from dolukpur. For my graduation, I chose Xyz program at Abc University. It was a very enriching experience at the University as not only we were actively involved in practical projects, but also got opportunities to participate in a number of sports and other extra-curricular activities such as plays and skits.</div>
+         <div classname=" grid grid-rows-3 gap-y-2 ">
+                        <button style={{ backgroundColor: "#3C3644" }} classname="text-white p-2 rounded-lg"> <i classname="bi bi-github"></i>  GitHub</button>
+                        <button style={{ backgroundColor: "#3C3644" }} classname="text-white p-2 rounded-lg"><i classname="bi bi-envelope"></i>   Email</button>
+                        <button style={{ backgroundColor: "#3C3644" }} classname="text-white p-2 rounded-lg"> <i classname="bi bi-instagram"></i> Instagram</button>
+           </div>
         </div>
-        <div class="stickers">
-        <img src="Group 4.png" style="height: 80px; margin-top: 60px;margin-left: 180px;">
-        <img src="Group 5.png" style="height: 120px; margin-top: 20px;">
-        <img src="Group 6.png" style="height: 90px; margin-top: 20px;">
-        <img src="Group 7.png" style="height: 120px; margin-top: 20px;">
-        <img src="Group 8.png" style="height: 80px; margin-top: 20px;">
-        <img src="Group 9.png" style="height: 120px; margin-top: 20px;">
+                <div classname="grid grid-cols-3 gap-x-96 " style={{ marginTop: "120px " }}>
+                    <button style={{ backgroundColor: "#3C3644", height: "80px", width: "80px" }} classname="text-white p-2 rounded-lg"> proposal</button>
+                    <button style={{ backgroundColor: "#3C3644", width: "80px" }} classname="text-white p-2 rounded-lg"> news</button>
+                    <button style={{ backgroundColor: "#3C3644", width: "80px" }} classname="text-white p-2 rounded-lg"> complain</button>
         </div>
-       
-      </div></div>
+                <div style={{ backgroundColor: "#490260", height: "120px", padding: "0px", margin: "0px", marginTop: "60px", width: "100%" }} classname="grid grid-cols-3">
+                    <img src="mist.png" style={{ height: "45%", marginTop: "30px", marginLeft: "60px " }}/>
+                    <div style={{ marginTop: "15px" }} >
+                <div classname="text-white">Manipal Information Security Team</div>
+                        <div classname="text-white" style={{ marginTop: "10px", color: "#BAA8C0" }}>Manipal Institute Of Technology </div>
+                        <div classname="text-white" style={{ color: "#BAA8C0" }}>Udupi, Karnataka 576104</div>
+            </div>
+            <div classname="grid grid-cols-6">
+                        <img src="Group 4.png" style={{ height: "90px", marginTop: "30px" }}/>
+                        <img src="Group 5.png" style={{ height: "120px" }}/>
+                        <img src="Group 6.png" style={{ height: "90px", marginTop: "30px " }}/>
+                        <img src="Group 7.png" style={{ height: "120px " }}/>
+                        <img src="Group 8.png" style={{ height: "90px", marginTop: "30px" }}/>
+                        <img src="Group 9.png" style={{ height: "120px" }}/>
+            </div>
+                                                                        
+                                                                            </div>
+                                                                            </div>
     )
 }
 export default Dashboard
